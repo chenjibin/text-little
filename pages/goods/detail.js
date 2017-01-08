bfa4b5019301fce485d0f9e422549be4009f7cbc
Francis <363840817@qq.com>
2017-01-08 11:50:12 +0800

    商品详情 购物车 初步完成suk选择相关展示

--- pages/goods/detail.js
+++ pages/goods/detail.js
@@ -24,43 +24,43 @@ Page({
             totalKucun: '933',
             eachKucun: [
                 {
-                    name: '灰40',
+                    name: '灰/40/240/透气/新款40/240',
                     num: 10
                 },
                 {
-                    name: '灰41',
+                    name: '灰/40/240/透气/新款41/245',
                     num: 0
                 },
                 {
-                    name: '灰42',
+                    name: '灰/40/240/透气/新款42/255',
                     num: 2
                 },
                 {
-                    name: '灰43',
+                    name: '灰/40/240/透气/新款43/260',
                     num: 0
                 },
                 {
-                    name: '灰45',
+                    name: '灰/40/240/透气/新款45/265',
                     num: 0
                 },
                 {
-                    name: '红40',
+                    name: '红/新款/时尚/潮流40/240',
                     num: 10
                 },
                 {
-                    name: '红41',
+                    name: '红/新款/时尚/潮流41/245',
                     num: 0
                 },
                 {
-                    name: '红42',
+                    name: '红/新款/时尚/潮流42/255',
                     num: 15
                 },
                 {
-                    name: '红43',
+                    name: '红/新款/时尚/潮流43/260',
                     num: 0
                 },
                 {
-                    name: '红45',
+                    name: '红/新款/时尚/潮流45/265',
                     num: 16
                 }
             ]
@@ -68,15 +68,16 @@ Page({
         skuOptions: [
             {
                 skuName: '颜色分类',
+                name: 'cate',
                 options: [
                     {
-                        name: '灰',
+                        name: '灰/40/240/透气/新款',
                         pic: 'http://img2.xyyzi.com/Upload/images/20160724/5794c70f29c68.jpg',
                         select: '',
                         out: ''
                     },
                     {
-                        name: '红',
+                        name: '红/新款/时尚/潮流',
                         pic: 'http://img.gx.xyyzi.com/wd/product/20151211/1449775985-2843F09D-CC55-439E-820B-30C4F21B8B24.jpg',
                         select: '',
                         out: ''
@@ -85,29 +86,30 @@ Page({
             },
             {
                 skuName: '鞋码',
+                name: 'size',
                 options: [
                     {
-                        name: '40',
+                        name: '40/240',
                         select: '',
                         out: ''
                     },
                     {
-                        name: '41',
+                        name: '41/245',
                         select: '',
                         out: ''
                     },
                     {
-                        name: '42',
+                        name: '42/255',
                         select: '',
                         out: ''
                     },
                     {
-                        name: '43',
+                        name: '43/260',
                         select: '',
                         out: ''
                     },
                     {
-                        name: '45',
+                        name: '45/265',
                         select: '',
                         out: ''
                     }
@@ -120,8 +122,14 @@ Page({
             flag: true
         },
         mainData: {
-            stateArr:[],
-            sendData: {}
+            desc: '请选择',
+            stateArr: [],
+            stock: '933',
+            sendData: {
+                cate: '',
+                size: '',
+                num: ''
+            }
         }
     },
     onLoad(options) {
@@ -140,25 +148,46 @@ Page({
         let skuData = this.data.skuOptions
         let optionNum = e.target.dataset.oid
         let eachNum = e.target.dataset.eid
+        let oName = e.target.dataset.oname
         let selectOne = skuData[optionNum]['options'][eachNum]
         if (selectOne['out'] === 'out-of-stock') return
         let flag = selectOne['select'] === ''
         let defaultPic = this.data.goodInfo.defaultPicc
         let optionName = e.target.dataset.option
         let statedata = this.data.mainData.stateArr[optionNum]
-        this.cancelAllSelect(skuData[optionNum]['options'], eachNum)
         if (flag) {
+            this.cancelAllSelect(skuData[optionNum]['options'], eachNum)
             let pic = selectOne['pic']
             if (pic) {
                 this.setData({
                     'goodInfo.defaultPic': pic
                 })
             }
-            
-            if(statedata) this.outHandler(statedata, skuData,'')
+
+
+            this.data.mainData.sendData[oName] = optionName
+
+            let cate = this.data.mainData.sendData['cate']
+            let size = this.data.mainData.sendData['size']
+
+            if (cate && size) {
+                let str = cate + size
+                this.data.goodInfo.eachKucun.forEach((item) => {
+                    if (item.name === str) {
+                        this.data.mainData.stock = item.num
+                    }
+                })
+                this.data.mainData.desc = '已选:'
+            }
+            this.setData({
+                'mainData': this.data.mainData
+            })
+
+
+            if (statedata) this.outHandler(statedata, skuData, '')
             selectOne['select'] = 'select'
             this.data.mainData.stateArr[optionNum] = optionName
-            this.outHandler(optionName, skuData,'out-of-stock')
+            this.outHandler(optionName, skuData, 'out-of-stock')
 
         } else {
             if (selectOne['pic']) {
@@ -166,8 +195,23 @@ Page({
                     'goodInfo.defaultPic': defaultPic
                 })
             }
+
+            this.data.mainData.sendData[oName] = ''
+            this.data.mainData.desc = '请选择'
+
+             this.data.mainData.stock = this.data.goodInfo.totalKucun
+
+
+            this.setData({
+                'mainData': this.data.mainData
+            })
+
+            this.setData({
+                'mainData.sendData': this.data.mainData.sendData
+            })
+
             selectOne['select'] = ''
-            this.outHandler(optionName, skuData,'')
+            this.outHandler(optionName, skuData, '')
         }
         this.setData({
             skuOptions: skuData
@@ -175,11 +219,11 @@ Page({
     },
     cancelAllSelect(value, eachNum) {
         value.forEach((item, index) => {
-           //  if (eachNum === index) return
+            //  if (eachNum === index) return
             item.select = ''
         })
     },
-    outHandler(name, skuData,calss) {
+    outHandler(name, skuData, calss) {
         let dataRaary = []
         let kucunArray = this.data.goodInfo.eachKucun
         kucunArray.forEach((item) => {

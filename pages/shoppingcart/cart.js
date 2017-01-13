0ae667f481fba8ce37f8b305a76bfe7563853df1
chenjibin <363840817@qq.com>
2017-01-13 23:57:50 +0800

    确认订单样式编写

--- pages/shoppingcart/cart.js
+++ pages/shoppingcart/cart.js
@@ -299,7 +299,7 @@ Page({
     let sendData = this.data.mainData.sendData
     let goodLength = sendData.oidArray.length
     if (goodLength) {
-      wx.navigateTo({
+      wx.redirectTo({
         url: '../trade/confirm'
       })
     } else {
--- pages/trade/confirm.js
+++ pages/trade/confirm.js
@@ -4,13 +4,23 @@ Page({
             totalPrice: 0
         },
         array: ['美国', '中国', '巴西', '日本'],
-        index:0,
+        index: 0,
         serverData: {
             addressInfo: {
                 person: '天天',
                 mobile: '15061997866',
-                address: '	地址：江苏省  连云港市  新浦区  海连东路42号杰瑞科技创意产业园E1号楼5楼'
-            }
+                address: '江苏省  连云港市  新浦区  海连东路42号杰瑞科技创意产业园E1号楼5楼'
+            },
+            goodInfo: [
+                {
+                    title: 'Adidas阿迪达斯女装短袖 2016夏新款透气运动 针织圆领T恤AZ9463',
+                    pic: 'http://img2.xyyzi.com/Upload/images/20160703/5778dedabbab1.jpg',
+                    price: '169.00',
+                    num: '2',
+                    property: '颜色:AZ9463  尺码:M（165/88A）'
+                }
+            ],
+            totalPrice:'169.00'
         }
     },
     onLoad() {

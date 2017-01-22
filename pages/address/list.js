52c69559d22acbc628bfa33fcecc8b9de1dd5a5a
Francis <363840817@qq.com>
2017-01-22 11:57:38 +0800

    管理地址流程重构，防止子页面超过5个不能继续路由的问题

--- pages/address/list.js
+++ pages/address/list.js
@@ -40,6 +40,10 @@ Page({
                 }
             ],
             defaultAddress: '34'
+        },
+        mainData: {
+            mainFlag: false,
+            formShowClass: ''
         }
     },
     chooseDefaultHandler(e) {
@@ -62,10 +66,36 @@ Page({
         })
 
     },
+    addAddress() {
+        wx.setNavigationBarTitle({
+            title: '新增收货地址'
+        })
+        this.showPop()
+    },
     editAddress(event) {
         let id = event.target.dataset.id
-        wx.navigateTo({
-            url: './edit?id=' + id,
+        wx.setNavigationBarTitle({
+            title: '编辑收货地址'
         })
-    }
+        this.showPop() 
+    },
+    cancelPop() {
+        wx.setNavigationBarTitle({
+            title: '管理收货地址'
+        })
+        this.setData({
+            'mainData': {
+                mainFlag: false,
+                formShowClass: ''
+            }
+        })
+    },
+    showPop() {
+        this.setData({
+            'mainData': {
+                mainFlag: true,
+                formShowClass: 'show'
+            }
+        })
+    },
 })

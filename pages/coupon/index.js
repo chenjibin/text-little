cef7b20bc6d7a8a7d0fce09507e0e959929db39a
Francis <363840817@qq.com>
2017-01-26 18:28:21 +0800

    我的优惠券样式优化

--- pages/coupon/index.js
+++ pages/coupon/index.js
@@ -1,3 +1,59 @@
 Page({
-    
+    data: {
+        serverData: {
+            couponData: [
+                {
+                    cid: '10',
+                    money: '5',
+                    desc: '满5.01元使用',
+                    bottomText: '幸运叶子官网',
+                    num: '101',
+                    left: '98'
+                },
+                {
+                    cid: '11',
+                    money: '20',
+                    desc: '满200元使用',
+                    bottomText: '幸运叶子官网',
+                    num: '110',
+                    left: '81'
+                },
+                {
+                    cid: '12',
+                    money: '5',
+                    desc: '满5.01元使用',
+                    bottomText: '幸运叶子官网',
+                    num: '120',
+                    left: '65'
+                },
+                {
+                    cid: '13',
+                    money: '5',
+                    desc: '满5.01元使用',
+                    bottomText: '幸运叶子官网',
+                    num: '130',
+                    left: '33'
+                }
+            ]
+        },
+        mainData: {
+
+        }
+    },
+    onLoad() {
+
+    },
+    onShow() {
+        this.leftNumHandler()
+    },
+    leftNumHandler() {
+        let couponData = this.data.serverData.couponData
+        couponData.forEach((item) => {
+            let leftNum = Math.ceil((item.left / item.num) * 100)
+            item.leftNum = leftNum
+        })
+        this.setData({
+            'serverData.couponData': couponData
+        })
+    }
 })
--- pages/coupon/my.js
+++ pages/coupon/my.js
@@ -1,3 +1,16 @@
 Page({
-    
+    onReady() {
+        wx.getLocation({
+            type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+            success: function (res) {
+                var latitude = res.latitude
+                var longitude = res.longitude
+                wx.openLocation({
+                    latitude: latitude,
+                    longitude: longitude,
+                    scale: 28
+                })
+            }
+        })
+    }
 })
